4fa27cc701deacb676c627eb1458a9e92823e51f
m0g56 <138621757+m0g56@users.noreply.github.com>
2025-12-14 21:34:18 +0000

    Boilerplate added for Cursor API calls

--- vsCodeExt/src/extension.ts
+++ vsCodeExt/src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import * as https from 'https';
 import * as budget from './budget';
-import { Memento } from "vscode";
+import { Memento } from 'vscode';
 import { stringify } from 'querystring';
 
 
