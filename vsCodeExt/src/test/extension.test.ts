95260dec8ee727eef434fe823b9650b5924cdd82
Max <cg24012@bristol.ac.uk>
2026-02-01 00:59:13 +0000

    Added basic initial tests, added publisher field to package.json and updated gitignore to not include .vscode-test folder (which is massive and breaks pushes

--- vsCodeExt/src/test/extension.test.ts
+++ vsCodeExt/src/test/extension.test.ts
@@ -3,7 +3,8 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as extension from '../extension';
+import { isFunctionDeclaration } from 'typescript';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -13,3 +14,40 @@ suite('Extension Test Suite', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
 });
+
+suite('Commands are registered', () => {
+	// gets all registered commands
+	let allCommands: string[];
+	setup(async () => {
+		// WHEN PUBLISHING, CHANGE PUBLISHER FIELD IN PACKAGE.JSON AND ALSO REPLACE 'development'
+		// IN LINE BELOW WITH NEW PUBLISHER NAME.
+		const ext = vscode.extensions.getExtension('development.vsCodeExt');
+		await ext?.activate(); // Ensure the extension is actually running
+		allCommands = await vscode.commands.getCommands(true);
+	});
+
+
+
+
+	test('clearStore exists and runs', () => {
+		const found = allCommands.indexOf('vsCodeExt.clearStore') > -1;
+		assert.strictEqual(found, true, 'Command was not registered!');
+	});
+
+	test('inputdisplay exists and runs', () => {
+		const found = allCommands.indexOf('vsCodeExt.inputdisplay') > -1;
+		assert.strictEqual(found, true, 'Command was not registered!');
+	});
+	test('openDashboard exists and runs', () => {
+		const found = allCommands.indexOf('vsCodeExt.openDashboard') > -1;
+		assert.strictEqual(found, true, 'Command was not registered!');
+	});
+	// below test is for checking failed tests fail. They do!
+
+	// test('missingCommand exists and runs', () => {
+	// 	const found = allCommands.indexOf('vsCodeExt.missingCommand') > -1;
+
+	// 	assert.strictEqual(found, true, 'Command was not registered!');
+	// });
+});
+
