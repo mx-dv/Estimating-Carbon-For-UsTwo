9fbbc9b48d24e479990a16809b367334068de96a
NiHao <hao.crystal.ni@gmail.com>
2026-02-02 14:30:09 +0000

    add comments

--- vsCodeExt/src/dashboard.ts
+++ vsCodeExt/src/dashboard.ts
@@ -61,6 +61,9 @@ const scriptUri = webview.asWebviewUri(
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Carbon Dashboard</title>
 <style>
+
+/* day mode color setting */
+
 :root{
    --base-color: white;
   --base-variant: #e8e9ed;
@@ -68,6 +71,8 @@ const scriptUri = webview.asWebviewUri(
   --secondary-text: #232738;
   --primary-color: #3a435d;
 }
+
+/* night mode color setting */
 
 body.darkmode{
     --base-color:#070b1d;
@@ -77,12 +82,19 @@ body.darkmode{
     --primary-color: #3a435d;
 }
 
+/* basic browser setting, and font */
+
 * { margin:0; padding:0; box-sizing:border-box; }
 html{ font-family: sans-serif; }
+
+/* main pag style, paddings  */
+
 body{ min-height:100vh; background-color:var(--base-color); color:var(--text-color); transition: all 0.3s ease; }
 header, section{ padding:70px min(50px,7%); }
 section{ background-color: var(--base-variant); }
 p{ margin:10px 0 20px 0; color:var(--secondary-text); }
+
+/* the circle outside icon */
 
 #theme-switch{
     height: 50px;
@@ -99,6 +111,8 @@ p{ margin:10px 0 20px 0; color:var(--secondary-text); }
     cursor: pointer;
 }
 
+/* hide icon at relevent mode */
+
 #theme-switch svg{ fill: var(--primary-color); }
 #theme-switch svg:last-child{ display: none; }
 body.darkmode #theme-switch svg:first-child{ display: none; }
@@ -107,19 +121,24 @@ body.darkmode #theme-switch svg:last-child{ display: block; }
 </head>
 <body>
 <header>
+
+<!-- icon picture link -->
+
   <button id="theme-switch">
     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z"/></svg>
     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z"/></svg>
   </button>
   <h1>chart 1</h1>
-  <p> soemthing here</p>
+  <p> something here</p>
 </header>
 <section>
   <h2>chart 2</h2>
   <p>something here</p>
 </section>
-
 <script>
+
+// listens for clicking and change color for it
+
   const btn = document.getElementById('theme-switch');
   btn.addEventListener('click', () => {
     document.body.classList.toggle('darkmode');
