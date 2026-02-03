9bd31b8833e23e0bd6ba3a6321cf7a82a992389f
Max <cg24012@bristol.ac.uk>
2026-02-03 14:51:54 +0000

    Updated the test file - some tests weren't being asserted and so were passing by default. Removed Unnecessary prints

--- vsCodeExt/src/test/extension.test.ts
+++ vsCodeExt/src/test/extension.test.ts
@@ -1,23 +1,6 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as extension from '../extension';
-import { isFunctionDeclaration } from 'typescript';
 import * as sinon from 'sinon';
-import { state } from '../state';
-import { appendFile } from 'fs';
-
-
-// suite('Extension Test Suite', () => {
-// 	vscode.window.showInformationMessage('Start all tests.');
-
-// 	test('Sample test', () => {
-// 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-// 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-// 	});
-// });
 
 suite("CommandTests", () => {
 	// gets all registered commands
@@ -35,18 +18,15 @@ suite("CommandTests", () => {
 		const myExtensionCommands = allCommands.filter(cmd => cmd.startsWith('ecode.'));
 		try {
 			for (const command of myExtensionCommands) {
-				console.log("Running: ${command}");
+				console.log(`Running: ${command}`);
 				if (command === "ecode.interceptorStart") {
 
 					await vscode.commands.executeCommand(command);
 					await new Promise(res => setTimeout(res, 500));
 					const status = dynamics.isInterceptorRunning();
 					assert.strictEqual(status, true, "Interceptor Not Running Correctly");
-					console.log("Interceptor is running in the background - it started correctly");
-				} else {
-					console.log("ELSE" + dynamics.isInterceptorRunning());
-				}
-
+				} else vscode.commands.executeCommand(command);
+				console.log(`Successfully Ran Test ${command}`);
 			}
 		} catch (error) {
 			assert.fail("Command failed to execute: ${error}");
@@ -60,11 +40,11 @@ suite("CommandTests", () => {
 });
 
 suite("UI Tests", () => {
-	
+
 });
 
 suite("RunTime Tests", () => {
-	
+
 });
 
 suite("DevTime Tests", () => {
